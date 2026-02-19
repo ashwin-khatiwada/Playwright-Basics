e71c633b1ef2334775114d860ad6839443324236
ashwin-khatiwada <ashwingithub74@gmail.com>
2026-02-19 16:26:07 +0545

    feat: adding negative scenario for Login

--- tests/orangehrm-login.spec.js
+++ tests/orangehrm-login.spec.js
@@ -8,3 +8,12 @@ test('Login to OrangeHRM successfully', async ({ loginPage, page }) => {
     await expect(page).toHaveURL(/.*dashboard/);
     await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
 });
+
+test('Negative Case: Login to OrangeHRM Fail', async ({ loginPage, page }) => {
+    await loginPage.goto();
+    await loginPage.login('Admin', 'asfsa');
+
+    // Verify invalid login
+    page.getByRole('alert').filter({ hasText: 'Invalid credentials' });
+
+});
